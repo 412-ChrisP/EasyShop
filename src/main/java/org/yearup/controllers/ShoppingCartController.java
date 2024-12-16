1129b61d325a5fcc5fb0c3469e6e4e039fcb9218
412-ChrisP <chrispbacon542@gmail.com>
2024-12-16 12:59:58 -0500

    finished clearCart in ShoppingCartController

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -94,4 +94,21 @@ public class ShoppingCartController
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart
 
+    @DeleteMapping
+    public void clearCart(Principal principal)
+    {
+        try
+        {
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            ShoppingCart cart = shoppingCartDao.getByUserId(userId);
+            cart.getItems().clear();
+        }
+        catch (Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error clearing cart.");
+        }
+    }
 }
